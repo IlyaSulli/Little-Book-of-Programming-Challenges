9492723a749f13484241608f57bb76c6a973d5de
sam-struthers <62945604+sam-struthers@users.noreply.github.com>
2022-03-17 12:40:51 +0000

    Update challenge4.py

--- challenge4.py
+++ challenge4.py
@@ -1,5 +1,13 @@
 print("Welcome to the distance calculator!")
-speed = input("What was your speed (m/s)? ")
-time = input("How long did it take you (s)? ")
+speed = int(input("What was your speed (m/s)? "))
+time = int(input("How long did it take you (s)? "))
 distance = speed * time
 print("You traveled ",distance," meters")
+
+#extension
+print("welcome to the speed calculator!")
+time = int(input("How long do you have (s)? "))
+distance = int(input("What distance do you want to travel (m)? "))
+speed = distance/time
+print("You would need to travel ",speed,"m/s to travel ",distance,"m in ", time,"s")
+
