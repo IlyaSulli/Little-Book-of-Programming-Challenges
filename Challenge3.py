08bffbb278f0b5180837b2dc477e05bdfd28061d
Ilya <81645040+IlyaSulli@users.noreply.github.com>
2022-03-17 12:33:47 +0000

    Create Challenge3.py

---
+++ Challenge3.py
@@ -0,0 +1,10 @@
+print("Area of Rectange")
+height = int(input("What is the height of the rectange: "))
+width = int(input("What is the width of the rectange: "))
+area = height*width
+print ("Area: " + Area)
+
+print ("Volume of a cuboid")
+depth = int(input("What is the depth of the cuboid"))
+volume = area*depth
+print ("Volume: " + volume)
